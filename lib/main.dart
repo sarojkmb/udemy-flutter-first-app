d3fa2ca37769417c171ad93979678ecbf72140ff
Saroj personal mac <sarojkmb@gmail.com>
2020-03-10 00:12:21 -0400

    dehid

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/Answer.dart';
+import 'package:flutter_first_app/question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState(){
+    //TODO implement create state
+    return _MyAppState();
+  }
+
+}
+
+class _MyAppState extends State<MyApp>{
+
+  var _questionIdex = 0;
+  var questions = [
+    'What is you favorite color?',
+    'What is you favorite animal?'
+  ];
+
+  void _answerQuestion() {
+    setState( (){
+      _questionIdex++;
+    });
+    
+    print('Answer chosen $_questionIdex');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('This is first App'),
+          title: Text("My first App"),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Question(questions[_questionIdex]),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+          ],
+        ),
       ),
     );
   }
